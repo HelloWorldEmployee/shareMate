a130bd3d46e0e7daf4f50d8feff0601a03bd2f70
jail <juji1007@hanmail.net>
2024-11-16 14:38:26 +0900

    fix_folder_structure

---
+++ Backend_spring-boot/src/test/java/com/sharemate/webservice/SharemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharemate.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
